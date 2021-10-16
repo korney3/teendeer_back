70ca95ddcca7532dc3c2b60abe2f160bcaf9021d
Alenicheva Alisa <koren.iz3x@yandex.ru>
2021-10-16 11:25:58 +0500

    Add

--- database/models.py
+++ database/models.py
@@ -34,46 +34,46 @@ class GiftedChild(models.Model):
 class User(models.Model):
     fullname = models.CharField(max_length=64, null=True, blank=True)
 
-    login = models.CharField(max_length=64, null=False, blank=False)
-    password = models.CharField(max_length=64, null=False, blank=False)
+    login = models.CharField(max_length=64, null=False, blank=False, default = "login")
+    password = models.CharField(max_length=64, null=False, blank=False, default = "login")
 
-    user_level = models.IntegerField(null=False, blank=False)
+    user_level = models.IntegerField(null=False, blank=False, default=1)
 
     bio = models.CharField(max_length=512, null=True, blank=True)
     school = models.CharField(max_length=512, null=True, blank=True)
     organizations = models.CharField(max_length=512, null=True, blank=True)
 
-    date_of_birth = models.DateTimeField(auto_now=False)
+    date_of_birth = models.DateTimeField(auto_now=False, null=True, blank = True)
 
 
 class Talent(models.Model):
     name = models.CharField(max_length=64, null=False, blank=False)
-    users = models.ManyToManyField(User)
+    users = models.ManyToManyField(User, blank=True)
 
 
 class Challenge(models.Model):
     challenge_name = models.CharField(max_length=64, null=False, blank=False)
     image_url = models.CharField(max_length=512, null=True, blank=True)
-    req_talent_level = models.IntegerField(primary_key=False)
-    talent = models.ForeignKey(Talent, on_delete=models.CASCADE)
-    max_tasks = models.IntegerField(primary_key=False)
+    req_talent_level = models.IntegerField(null=False, blank=False,  default=1)
+    talent = models.ForeignKey(Talent, on_delete=models.CASCADE, null=False, blank = False)
+    max_tasks = models.IntegerField(null=False, blank=False,  default=1)
     description = models.CharField(max_length=512, null=True, blank=True)
 
 
 class Task(models.Model):
-    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE)
+    challenge = models.ForeignKey(Challenge, on_delete=models.CASCADE, null=False, blank=False)
     task_name = models.CharField(max_length=64, null=False, blank=False)
     description = models.CharField(max_length=512, null=True, blank=True)
     image_url = models.CharField(max_length=512, null=True, blank=True)
-    task_points = models.IntegerField(primary_key=False)
-    max_steps = models.IntegerField(primary_key=False)
-    task_number = models.IntegerField(primary_key=False)
+    task_points = models.IntegerField(null=False, blank=False,  default=0)
+    max_steps = models.IntegerField(null=False, blank=False,  default=1)
+    task_number = models.IntegerField(null=False, blank=False,  default=1)
 
 
 class Step(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     step_name = models.CharField(max_length=64, null=False, blank=False)
-    step_number = models.IntegerField(primary_key=False)
+    step_number = models.IntegerField(null=False, blank=False,  default=1)
     step_text = models.CharField(max_length=512, null=True, blank=True)
     image_url = models.CharField(max_length=512, null=True, blank=True)
     button_text = models.CharField(max_length=512, null=True, blank=True)
